7d3212d0d6bed9c02f75bc75bc4e857b6951caf8
neverFeltAlive <20This.Is.My.Mail00@proronmail.com>
2023-06-21 22:19:25 +0300

    [feature] - updated config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
-    root: true,
-    env: {
-        node: true,
-        browser: true,
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:vue/vue3-essential',
-        '@nuxt/eslint-config',
-        'plugin:prettier/recommended',
-    ],
-    rules: {
-        'vue/component-name-in-template-casing': ['error', 'kebab-case'],
-        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    },
-    globals: {
-        $nuxt: true,
-    },
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+  },
+  extends: [
+    'eslint:recommended',
+    '@nuxt/eslint-config',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'vue/component-name-in-template-casing': ['error', 'kebab-case'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': 0,
+  },
+  globals: {
+    $nuxt: true,
+  },
 };
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
+
 export default defineNuxtConfig({
   modules: ['@nuxtjs/stylelint-module', '@nuxtjs/eslint-module'],
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: [
+      join(currentDir, "./assets/styles/main.scss")
+  ],
 })
